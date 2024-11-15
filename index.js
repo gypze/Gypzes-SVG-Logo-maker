a042254c4d4cc839a3f346bbd37379053b080c5b
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-11-14 21:39:53 -0600

    I updated the index.js

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Runs the application using imports from lib/ 
+
+// index.js
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+
+const questions = [
+{
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to three characters to render for the logo text:',
+    validate: input => input.length <= 3 || 'Text must be up to three characters.'
+},
+ {
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter the text color (keyword or hexadecimal):'
+},
+{
+    type: 'list',
+    name: 'shape',
+    message: 'Choose a shape for the logo:',
+    choices: ['Circle', 'Triangle', 'Square']
+},
+{
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter the shape color (keyword or hexadecimal):'
+}
+];
+
+inquirer.prompt(questions).then(answers => {
+    let shape;
+    switch (answers.shape) {
+        case 'Circle':
+            shape = new Circle();
+            break;
+        case 'Triangle':
+            shape = new Triangle();
+            break;
+        case 'Square':
+            shape = new Square();
+            break;
+    }
+    shape.setColor(answers.shapeColor);
+
+    const svgContent = `
+    <svg width="300" height="200" xmlns+"http://www.w3.org/2000/svg">
+    ${shape.render()}
+    <text x="150" y=125 font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}<
+    </svg>
+
+    fs.writeFileSync('logo.svg', svgContent.trim());
+    console.log('Generated logo.svg'):
+});
